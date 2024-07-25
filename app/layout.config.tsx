b786edff4002603ac4b11298e3f43025bf774ecd
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-07-25 12:15:09 +0530

    smol fixes before major redirects

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,6 +1,8 @@
 import { type BaseLayoutProps, type DocsLayoutProps } from 'fumadocs-ui/layout';
 import { Title } from '@/app/layout.client';
 import { pageTree } from '@/app/source';
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
+import { BlocksIcon, MailIcon, SproutIcon, SquareStackIcon } from 'lucide-react';
 
 // shared configuration
 export const baseOptions: BaseLayoutProps = {
@@ -13,4 +15,37 @@ export const baseOptions: BaseLayoutProps = {
 export const docsOptions: DocsLayoutProps = {
   ...baseOptions,
   tree: pageTree,
+  sidebar: {
+    defaultOpenLevel: 0,
+    banner: (
+      <RootToggle
+        options={[
+          {
+            title: 'Learn',
+            description: '',
+            icon: <SproutIcon />,
+            url: '/docs/learn/what-is-avalanche',
+          },
+          {
+            title: 'Multi-Chain Architecture',
+            description: '',
+            icon: <SquareStackIcon />,
+            url: '/docs/api-reference',
+          },
+          {
+            title: 'AWM & Teleporter',
+            description: '',
+            icon: <MailIcon />,
+            url: '/docs/build/cross-chain/avalanche-warp-messaging/overview',
+          },
+          {
+            title: 'Customizing EVM',
+            description: '',
+            icon: <BlocksIcon />,
+            url: '/course/customizing-evm',
+          },
+        ]}
+      />
+    ),
+  }
 };
