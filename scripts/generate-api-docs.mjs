83b84dc84ba09a870abebc214c3c6c9a2d26c5a6
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-08-02 17:06:58 +0530

    update api playground

--- scripts/generate-api-docs.mjs
+++ scripts/generate-api-docs.mjs
@@ -2,5 +2,5 @@ import { generateFiles } from 'fumadocs-openapi';
  
 void generateFiles({
   input: ['./scripts/*.json'],
-  output: './content/docs/api-reference/playground',
+  output: './content/docs/api-playground',
 });
